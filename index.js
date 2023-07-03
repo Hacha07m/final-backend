ca7822a3119474945f5eaa4ca97fca16da6cab1a
mohamed <mohamedhadd95@gmail.com>
2023-07-03 18:29:37 +0100

    fourth commit

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ const corsOptions ={
 
 
 //database connection
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 const connect = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology :true
+            // useNewUrlParser: true,
+            // useUnifiedTopology :true
         })
 
         console.log('MongoDB database connected');
