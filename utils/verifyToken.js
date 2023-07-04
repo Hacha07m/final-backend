aad4608a9e0e506ee870d954e4516d7882af50e5
mohamed <mohamedhadd95@gmail.com>
2023-07-04 20:33:16 +0100

    co

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -6,7 +6,7 @@ export const verifyToken = (req,res,next)=>{
     const token = req.cookies.accessToken
     console.log(req.cookies)
     if(!token){
-        return res.status(401).json({success: false, message:"You're not authorize"})
+        return res.status(401).json({success: false, message:"You're not authorized"})
     }
 
 
